789828f6b6ec8a2f33d76deab87ba9bbea0a48c9
Nathan Simon <nsimon06@uw.edu>
2026-01-19 15:30:39 -0800

    everything but customs

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -28,6 +28,95 @@ print("Welcome back to the UW Calculator")
 //: IMPORTANT: If any tests are commented out, you will be graded a zero (0)! You should never be in the habit of eliminating tests to make the code pass.
 //:
 class Calculator {
+    
+    
+    //constructor
+    init() {
+    
+    }
+    
+//ELEMENTARY FUNCTIONS ----------------------------
+    
+    //ADD -------------------
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func add(_ args: [Int]) -> Int {
+        var ret = 0;
+        
+        for i in args {
+            ret += i
+        }
+        
+        return ret
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": lhs["x"]! + rhs["x"]!, "y": lhs["y"]! + rhs["y"]!]
+    }
+    
+    //END ADD -------------------------------
+    
+    //SUBTRACT ----------------------------
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": lhs["x"]! - rhs["x"]!, "y": lhs["y"]! - rhs["y"]!]
+    }
+    //END SUBTRACT -----------------------
+    
+    //MULTIPLY
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func multiply(_ args: [Int]) -> Int {
+        var ret = 1;
+        
+        for i in args {
+            ret *= i
+        }
+        
+        return ret
+    }
+    //END MULTIPLY -----------------------
+    
+    //DIVIDE -----------------------------
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+//END ELEMENTARY FUNCTIONS ---------------------------
+    
+    func count(_ args: [Int]) -> Int {
+        return args.count
+    }
+    
+    func avg(_ args: [Int]) -> Int {
+        var ret = 0;
+        
+        for i in args {
+            ret += i
+        }
+        
+        return ret/args.count
+    }
+//CUSTOM MATHOPS
+    
+    
+    
+    
 }
 
 //: Don't change the name of this object (`calc`); it's used in all the tests.
@@ -52,10 +141,13 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
+
+/*
 calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
+ */
 
 calc.add([1, 2, 3, 4, 5]) == 15
 calc.multiply([1, 2, 3, 4, 5]) == 120
@@ -65,13 +157,14 @@ calc.avg([2, 2, 2, 2, 2, 2]) == 2
 calc.avg([1, 2, 3, 4, 5]) == 3
 calc.avg([1]) == 1
 
+/*
 calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
     // this is (((0 op 1) op 2) op 3)
 calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
     // this is (((((0 op 1) op 2) op 3) op 4) op 5)
 calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
     // this is (((((1 op 1) op 1) op 1) op 1) op 1)
-
+*/
 let p1 = (5, 5)
 let p2 = (12, -27)
 let p3 = (-4, 4)
@@ -80,6 +173,7 @@ calc.add(lhs: p1, rhs: p2) == (17, -22)
 calc.subtract(lhs: p1, rhs: p2) == (-7, 32)
 calc.add(lhs: p4, rhs: p4) == (0, 0)
 calc.add(lhs: p3, rhs: p4) == (-4, 4)
+
 
 let pd1 = ["x": 5, "y": 5]
 let pd2 = ["x": -4, "y": 4]
